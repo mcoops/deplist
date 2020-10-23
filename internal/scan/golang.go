feabf01795d37c40c08d1fd3ee52f606914bc4d7
Jason Shepherd <jason@jasonshepherd.net>
2020-10-23 15:51:23 +1000

    added test for err first

--- internal/scan/golang.go
+++ internal/scan/golang.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
 
 	"golang.org/x/tools/go/packages"
 )
@@ -20,31 +21,35 @@ func GetGolangDeps(path string) ([]*packages.Package, error) {
 
 	cfg := &packages.Config{Mode: defaultOptions, Dir: dirPath}
 
-	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() && info.Name() == "vendor" {
-			return filepath.SkipDir
-		}
-		if info.Name() == "go.mod" {
-			currentDir := filepath.Dir(path)
-			if _, err := os.Stat(currentDir + "/vendor"); !os.IsNotExist(err) {
-				fmt.Printf("component has both go.mod and vendor, forcing use of vendor")
-				cfg.Env = append(os.Environ(), "GOFLAGS=-mod=vendor")
-				return nil
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		fmt.Printf("error walking the path %q: %v, trying to load deps anyway\n", dirPath, err)
-	}
-
 	pkgs, err := packages.Load(cfg, "./...")
 
 	if err != nil {
-		return nil, err
+		if strings.Contains(err.Error(), "invalid pseudo-version") {
+			err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if info.IsDir() && info.Name() == "vendor" {
+					return filepath.SkipDir
+				}
+				if info.Name() == "go.mod" {
+					currentDir := filepath.Dir(path)
+					if _, err := os.Stat(currentDir + "/vendor"); !os.IsNotExist(err) {
+						fmt.Printf("component has both go.mod and vendor, forcing use of vendor")
+						cfg.Env = append(os.Environ(), "GOFLAGS=-mod=vendor")
+						return nil
+					}
+				}
+				return nil
+			})
+			if err != nil {
+				fmt.Printf("error walking the path %q: %v, trying to load deps anyway\n", dirPath, err)
+			} else {
+				pkgs, err = packages.Load(cfg, "./...")
+			}
+		} else {
+			return nil, err
+		}
 	}
 
 	// based off the original https://github.com/golang/tools/blob/e140590b16906206021525faa5a48c7314806569/go/packages/gopackages/main.go#L99
