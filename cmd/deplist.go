1a74bfc8033fee089c8b9fcc43882ffc66597204
mcoops <cooper.d.mark@gmail.com>
2021-04-01 22:24:04 +1000

    cmd/deplist: fix so that it prints everything by default
    Signed-off-by: mcoops <cooper.d.mark@gmail.com>

--- cmd/deplist.go
+++ cmd/deplist.go
@@ -8,19 +8,26 @@ import (
 )
 
 func main() {
-	deptypePtr := flag.Int("deptype", deplist.LangGolang, "golang, nodejs, python etc")
+	deptypePtr := flag.Int("deptype", -1, "golang, nodejs, python etc")
 
 	flag.Parse()
 	path := flag.Args()[0]
-	deptype := deplist.Bitmask(*deptypePtr)
 
 	deps, _, err := deplist.GetDeps(path)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	for _, dep := range deps {
-		if (dep.DepType & deptype) == deptype {
+
+	if *deptypePtr == -1 {
+		for _, dep := range deps {
 			fmt.Printf("%s %s\n", dep.Path, dep.Version)
+		}
+	} else {
+		deptype := deplist.Bitmask(*deptypePtr)
+		for _, dep := range deps {
+			if (dep.DepType & deptype) == deptype {
+				fmt.Printf("%s %s\n", dep.Path, dep.Version)
+			}
 		}
 	}
 }
